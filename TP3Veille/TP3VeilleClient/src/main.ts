91294647b775151a1e902358d2e4b43dfac0b033
Renaud Lussier <renaud_lussier90@hotmail.com>
2022-07-14 10:39:15 -0400

    fix deployment

--- TP3Veille/TP3VeilleClient/src/main.ts
+++ TP3Veille/TP3VeilleClient/src/main.ts
@@ -6,7 +6,7 @@ import { VueSignalR } from "@quangdao/vue-signalr";
 
 createApp(App)
   .use(VueSignalR, {
-    url: "https://localhost:7242/chat" /*'http://localhost:5062'*/,
+    url: "https://tp3veilleserveur.azurewebsites.net/chat" /*"https://localhost:7242/chat"*/ /*'http://localhost:5062'*/,
   })
   .use(store)
   .use(router)
