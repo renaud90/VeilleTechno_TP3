d65b9de8d3dd6223379fc191be27e4f25f0893a5
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-07-13 20:25:55 -0400

    Ajout du service des conversations

--- TP3Veille/TP3VeilleSignalR/Data/Models/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TP3VeilleSignalR.Data.Models;
-
-public class Message
-{
-    public string Content { get; set; }
-    public string UserId { get; set; }
-    public string ConversationId { get; set; }
-    public DateTime Moment { get; set; }
-}
